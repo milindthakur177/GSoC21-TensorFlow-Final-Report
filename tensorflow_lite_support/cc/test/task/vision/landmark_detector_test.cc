b47f6cebc364e1b5e8826614a26d2dd267756bd0
Milind Thakur <milind170799@gmail.com>
2021-08-17 18:29:57 +0530

    Update landmark_detector_test.cc

--- tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
+++ tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
@@ -97,7 +97,7 @@ TEST_F(DetectTest, SucceedsWithFloatModel) {
   avg_score = total_score/17;
   EXPECT_NEAR(avg_score, SCORE, 0.1);
   */
-  EXPECT_NEAR(result.landmarks.score(), SCORE, 0.1);
+  EXPECT_NEAR(result.landmarks().score(), SCORE, 0.1);
 
 }
 
