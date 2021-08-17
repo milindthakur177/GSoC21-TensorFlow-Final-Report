8781cb01e8f5c8a396ea5e0e03af22053d698b38
Milind Thakur <milind170799@gmail.com>
2021-08-17 12:30:38 +0530

    Update landmark_detector_test.cc

--- tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
+++ tensorflow_lite_support/cc/test/task/vision/landmark_detector_test.cc
@@ -106,13 +106,12 @@ TEST_F(DetectTest, SucceedsWithFloatModel) {
   }
   avg_score = total_score/17;
   EXPECT_NEAR(avg_score, expect_score, 0.2);
-  /*
+  
   for (int i=0 ; i<17 ; ++i){
     EXPECT_NEAR(golden_x[i], key_x_golden[i],0.2);
     EXPECT_NEAR(golden_y[i], key_y_golden[i],0.2);
-    EXPECT_NEAR(golden_score[i], score_golden[i],0.2);
   }
-*/
+
 }
 
 class CreateFromOptionsTest : public tflite_shims::testing::Test {};
