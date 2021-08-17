955f92c8bc0e58fc9d375824d74e5517edda2697
Milind Thakur <milind170799@gmail.com>
2021-08-17 18:59:09 +0530

    Update landmark_detector.cc

--- tensorflow_lite_support/cc/task/vision/landmark_detector.cc
+++ tensorflow_lite_support/cc/task/vision/landmark_detector.cc
@@ -132,10 +132,10 @@ StatusOr<LandmarkResult> LandmarkDetector::Postprocess(
   const float* outputs = AssertAndReturnTypedTensor<float>(output_tensor);
 	
   LandmarkResult result;
-  Landmark* landmarks = result.add_landmarks();
-	for(int i =0 ; i<num_keypoints ; ++i){
+  
+	for(int i =0 ; i<17 ; ++i){
 
-    auto* landmarks = result.add_landmarks();
+    Landmark* landmarks = result.add_landmarks();
 
 		landmarks->set_key_y(outputs[3*i+0]) ;
 		landmarks->set_key_x(outputs[3*i+1]) ;
@@ -145,7 +145,7 @@ StatusOr<LandmarkResult> LandmarkDetector::Postprocess(
 
   }
 
-  landmarks->set_score(total_score/num_keypoints);
+  //landmarks->set_score(total_score/num_keypoints);
   return result;
 }
 
