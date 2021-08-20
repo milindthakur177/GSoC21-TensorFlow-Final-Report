371d74f16cf6a50f413d2d9970e9a294aa2af602
Milind Thakur <milind170799@gmail.com>
2021-08-20 12:44:06 +0530

    Update landmarks_proto_inc.h

--- tensorflow_lite_support/cc/task/vision/proto/landmarks_proto_inc.h
+++ tensorflow_lite_support/cc/task/vision/proto/landmarks_proto_inc.h
@@ -16,7 +16,5 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_SUPPORT_CC_TASK_VISION_PROTO_LANDMARKS_PROTO_INC_H_
 #define TENSORFLOW_LITE_SUPPORT_CC_TASK_VISION_PROTO_LANDMARKS_PROTO_INC_H_
 
-#include "tensorflow_lite_support/cc/task/vision/proto/class_proto_inc.h"
-
 #include "tensorflow_lite_support/cc/task/vision/proto/landmarks.pb.h"
 #endif  // TENSORFLOW_LITE_SUPPORT_CC_TASK_VISION_PROTO_LANDMARKS_PROTO_INC_H_
